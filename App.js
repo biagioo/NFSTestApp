9efbc8cedb49ed1e0cedb25516edbd4a720a7714
biagioo <mendolia2328@gmail.com>
2021-04-05 13:16:39 -0400

    added header component

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
+import Header from './src/components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
+      <Header />
       <Home />
     </View>
   );
@@ -14,6 +16,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
   },
 });
 

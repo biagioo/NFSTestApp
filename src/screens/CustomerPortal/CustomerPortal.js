0f4afacb4c7dd55ba81029c878de44fb3cacf1ee
biagioo <mendolia2328@gmail.com>
2021-04-15 07:32:11 -0400

    added Loading and Dashboard screen navigation routes

--- src/screens/CustomerPortal/CustomerPortal.js
+++ src/screens/CustomerPortal/CustomerPortal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
+import DashboardScreen from './DashboardScreen';
+import LoadingScreen from './LoadingScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +12,8 @@ const CustomerPortal = () => {
     <Stack.Navigator headerMode='none'>
       <Stack.Screen name='Log In' component={LoginScreen} />
       <Stack.Screen name='Register' component={RegisterScreen} />
+      <Stack.Screen name='Dashboard' component={DashboardScreen} />
+      <Stack.Screen name='Loading' component={LoadingScreen} />
     </Stack.Navigator>
   );
 };

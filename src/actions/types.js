7de9c01b5a1bcf1331ffb50639a6256118541148
biagioo <mendolia2328@gmail.com>
2021-04-19 09:32:53 -0400

    created types

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+export const GET_USER = 'GET_USER';
+export const GET_CUSTOMERS = 'GET_CUSTOMERS';
+export const SET_LOADING = 'SET_LOADING';
+export const USER_ERROR = 'USER_ERROR';
+export const USERS_ERROR = 'USERS_ERROR';
+
+// Chat actions here

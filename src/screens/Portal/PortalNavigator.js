7ca291ced40b08f650157dbe9ad76e61b7723a59
biagioo <mendolia2328@gmail.com>
2021-04-19 09:34:16 -0400

    implemented redux provider

--- src/screens/Portal/PortalNavigator.js
+++ src/screens/Portal/PortalNavigator.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+
 import DashboardScreen from './DashboardScreen';
 import LoadingScreen from './LoadingScreen';
 import CustomerDashboardScreen from './CustomerPortal/CustomerDashboardScreen';
 import AdminDashboardScreen from './AdminPortal/AdminDashboardScreen';
 import LoginScreen from './auth/LoginScreen';
 import RegisterScreen from './auth/RegisterScreen';
+import store from '../../store';
 
 const Stack = createStackNavigator();
 
 const PortalNavigator = () => {
   return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name='Log In' component={LoginScreen} />
-      <Stack.Screen name='Register' component={RegisterScreen} />
-      <Stack.Screen name='Dashboard' component={DashboardScreen} />
-      <Stack.Screen name='Loading' component={LoadingScreen} />
-      <Stack.Screen
-        name='CustomerDashboard'
-        component={CustomerDashboardScreen}
-      />
-      <Stack.Screen name='AdminDashboard' component={AdminDashboardScreen} />
-    </Stack.Navigator>
+    <Provider store={store}>
+      <Stack.Navigator headerMode='none'>
+        <Stack.Screen name='Log In' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Dashboard' component={DashboardScreen} />
+        <Stack.Screen name='Loading' component={LoadingScreen} />
+        <Stack.Screen
+          name='CustomerDashboard'
+          component={CustomerDashboardScreen}
+        />
+        <Stack.Screen name='AdminDashboard' component={AdminDashboardScreen} />
+      </Stack.Navigator>
+    </Provider>
   );
 };
 

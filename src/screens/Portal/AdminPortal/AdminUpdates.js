fbaa7c939f7868588345928f727b62accd207997
biagioo <mendolia2328@gmail.com>
2021-04-20 14:32:49 -0400

    created updates screens

---
+++ src/screens/Portal/AdminPortal/AdminUpdates.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AdminUpdates = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Admin Updates</Text>
+    </View>
+  );
+};
+
+export default AdminUpdates;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

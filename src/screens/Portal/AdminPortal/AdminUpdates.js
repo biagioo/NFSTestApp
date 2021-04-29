361d20baac6ed8b11aa34000541016325a0906f8
biagioo <mendolia2328@gmail.com>
2021-04-29 14:04:34 -0400

    retrieved users and displayed users from backend

--- src/screens/Portal/AdminPortal/AdminUpdates.js
+++ src/screens/Portal/AdminPortal/AdminUpdates.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import { ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import CustomListItem from '../../../components/Portal/CustomListItem';
+import firebase from 'firebase';
 
 const AdminUpdates = () => {
+  const users = useSelector(state => state.user.users);
+  const auth = useSelector(state => state.auth);
+  const initChat = customerId => {
+    const groupIds = {
+      adminId: firebase.auth().currentUser.uid,
+      customerId,
+    };
+  };
+
+  if (users === []) {
+    <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator size='large' color='#00ff00' />
+    </View>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Admin Updates</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+      {users.map((user, index) => (
+        <CustomListItem
+          key={index}
+          index={index}
+          user={user}
+          initChat={initChat}
+        />
+      ))}
+    </ScrollView>
   );
 };
 
 export default AdminUpdates;
 
 const styles = StyleSheet.create({
-  container: {
+  scrollViewContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '15%',
+  },
+
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
   },
 });

88615206a0f002e9fa08275dbf9df7e56031e222
biagioo <mendolia2328@gmail.com>
2021-04-29 09:43:34 -0400

    created user actions and user reducer

---
+++ src/actions/user.actions.js
@@ -0,0 +1,24 @@
+import { GET_REALTIME_USERS } from './types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+export const getRealtimeUsers = uid => {
+  return async dispatch => {
+    const db = firebase.firestore();
+    db.collection('users').onSnapshot(querySnapshot => {
+      const users = [];
+      querySnapshot.forEach(doc => {
+        if (doc.data().uid !== uid) {
+          users.push(doc.data());
+        }
+      });
+      // console.log(users);
+
+      dispatch({
+        type: GET_REALTIME_USERS,
+        payload: { users },
+      });
+    });
+  };
+};

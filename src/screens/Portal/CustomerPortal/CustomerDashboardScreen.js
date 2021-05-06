2ed25609a4cfc388dc9b5ff80c008b5b18838aa8
biagioo <mendolia2328@gmail.com>
2021-05-06 14:08:07 -0400

    removed updates tab

--- src/screens/Portal/CustomerPortal/CustomerDashboardScreen.js
+++ src/screens/Portal/CustomerPortal/CustomerDashboardScreen.js
@@ -3,7 +3,6 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import CustomerProfile from './CustomerProfile';
 import CustomerChats from './CustomerChats';
-import CustomerUpdatesScreen from './CustomerUpdatesScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +11,6 @@ const CustomerDashboardScreen = () => {
     <Tab.Navigator>
       <Tab.Screen name='Customer Profile' component={CustomerProfile} />
       <Tab.Screen name='Customer Chat' component={CustomerChats} />
-      <Tab.Screen name='Customer Updates' component={CustomerUpdatesScreen} />
     </Tab.Navigator>
   );
 };

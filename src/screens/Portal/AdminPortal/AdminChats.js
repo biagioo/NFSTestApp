81b277a365600ecd00d80f23359d524388456442
biagioo <mendolia2328@gmail.com>
2021-05-15 11:13:15 -0400

    added profilePic

--- src/screens/Portal/AdminPortal/AdminChats.js
+++ src/screens/Portal/AdminPortal/AdminChats.js
@@ -18,6 +18,7 @@ const AdminUpdates = ({ navigation }) => {
       customerEmail: customer.email,
       customerName: customer.name,
       customerVinNumb: customer.vinNumber,
+      profilePic: customer.profilePic,
       token: customer.token,
     };
 

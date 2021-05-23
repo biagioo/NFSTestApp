f5e9fd8ec65ca219b150bdc5007888446785b57f
biagioo <mendolia2328@gmail.com>
2021-05-23 07:09:23 -0400

    added icons

--- src/screens/Portal/AdminPortal/AdminDashboardScreen.js
+++ src/screens/Portal/AdminPortal/AdminDashboardScreen.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Ionicons } from '@expo/vector-icons';
 import AdminProfile from './AdminProfile';
 import AdminChats from './AdminChats';
-// import AdminUpdatesScreen from './AdminUpdatesScreen';
 
 const Tab = createBottomTabNavigator();
 
 const AdminDashboardScreen = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Admin Profile' component={AdminProfile} />
-      <Tab.Screen name='Admin Chat' component={AdminChats} />
-      {/* <Tab.Screen name='Admin Updates' component={AdminUpdatesScreen} /> */}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-circle-outline';
+          } else if (route.name === 'Updates') {
+            iconName = focused
+              ? 'chatbubble-ellipses'
+              : 'chatbubble-ellipses-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name='Profile' component={AdminProfile} />
+      <Tab.Screen name='Updates' component={AdminChats} />
     </Tab.Navigator>
   );
 };
--- src/screens/Portal/CustomerPortal/CustomerDashboardScreen.js
+++ src/screens/Portal/CustomerPortal/CustomerDashboardScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Ionicons } from '@expo/vector-icons';
 import CustomerProfile from './CustomerProfile';
 import CustomerChats from './CustomerChats';
 
@@ -8,9 +8,29 @@ const Tab = createBottomTabNavigator();
 
 const CustomerDashboardScreen = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Customer Profile' component={CustomerProfile} />
-      <Tab.Screen name='Customer Chat' component={CustomerChats} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-circle-outline';
+          } else if (route.name === 'Updates') {
+            iconName = focused
+              ? 'chatbubble-ellipses'
+              : 'chatbubble-ellipses-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name='Profile' component={CustomerProfile} />
+      <Tab.Screen name='Updates' component={CustomerChats} />
     </Tab.Navigator>
   );
 };

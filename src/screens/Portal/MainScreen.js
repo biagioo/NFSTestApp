0c2b7ea119b29a65a1cbd3b32f7ff659353500d9
biagioo <mendolia2328@gmail.com>
2021-06-01 07:53:50 -0400

    added styling

--- src/screens/Portal/MainScreen.js
+++ src/screens/Portal/MainScreen.js
@@ -6,6 +6,9 @@ import { Button } from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSignedInUser } from '../../actions';
 
+import { Image } from 'react-native';
+import { screenHeight, screenWidth } from '../../GlobalStyles';
+
 const MainScreen = props => {
   const auth = useSelector(state => state.auth);
   const { navigation } = props;
@@ -33,13 +36,24 @@ const MainScreen = props => {
   return (
     <View style={styles.container}>
       <StatusBar style='dark' />
-      <View style={styles.header}>{/* nfs logo here */}</View>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require('../../../assets/images/NFSLogo.png')}
+          resizeMode='contain'
+        />
+        <Text style={styles.title}>Welcome to the Performance Portal!</Text>
+      </View>
       <View style={styles.body}>
-        <Text>Welcome to the Performance Portal!</Text>
-        <Text>
+        <Text style={styles.bodyText}>
           Please keep all conversations strictly about the progress of your
           vehicle.
         </Text>
+        <Text style={styles.bodyText}>
+          Thanks for choosing Need For Speed NY.
+        </Text>
+      </View>
+      <View style={styles.footer}>
         <Button title='Enter the Performance Portal' onPress={enterPortal} />
       </View>
     </View>
@@ -55,16 +69,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   header: {
-    flex: 1,
-  },
-  body: {
     flex: 2,
   },
-  data: {
+  logo: {
+    marginTop: '12%',
+    marginHorizontal: '3%',
+    width: screenWidth - 30,
+    height: screenHeight / 6,
+  },
+
+  title: {
+    fontSize: 30,
+    paddingHorizontal: '5%',
+    fontWeight: '700',
+    fontStyle: 'italic',
+  },
+  body: {
+    flex: 3,
+    paddingHorizontal: '5%',
+  },
+  bodyText: {
+    fontSize: 22,
+    fontWeight: '400',
+    padding: 10,
+  },
+  footer: {
     flex: 1,
-    backgroundColor: 'green',
-    width: 400,
-    height: 1000,
-    bottom: 100,
   },
 });
